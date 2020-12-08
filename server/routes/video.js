21803a6095a9c31cfbcbd8ee7e0a5b17ac56723d
lord <marialindananono@gmail.com>
2020-12-08 08:08:25 +0300

    url

--- server/routes/video.js
+++ server/routes/video.js
@@ -48,7 +48,6 @@ router.post("/thumbnail", (req, res) => {
 
   ffmpeg.ffprobe(req.body.filePath, function (err, metadata) {
     console.dir(metadata);
-    console.log(metadata);
     console.log(metadata.format.duration);
 
     fileDuration = metadata.format.duration;
