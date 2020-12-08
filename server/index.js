a489a85e2d53b0248145783c4f49b2013e185bea
lord <marialindananono@gmail.com>
2020-12-08 06:07:23 +0300

    add path

--- server/index.js
+++ server/index.js
@@ -4,13 +4,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const config = require("./config/key");
-
+const path = require("path");
 // const mongoose = require("mongoose");
 // mongoose
 //   .connect(config.mongoURI, { useNewUrlParser: true })
 //   .then(() => console.log("DB connected"))
 //   .catch(err => console.error(err));
-console.log();
 const mongoose = require("mongoose");
 const connect = mongoose
   .connect(process.env.MONGO_URL, {
